daf4c7c7c4c1121869169317d3f65f405ea85990
Abdallah Dheir <73731965+powermski@users.noreply.github.com>
2022-03-02 20:33:40 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+    <link rel="icon" href="img/logo.png" />
+    <title>power-mski</title>
+    <script defer="defer" src="js/chunk-vendors.1e6514a4.js"></script>
+    <script defer="defer" src="js/app.97a721fd.js"></script>
+    <link href="css/chunk-vendors.cc486695.css" rel="stylesheet" />
+    <link href="css/app.1b501b56.css" rel="stylesheet" />
+    
+  </head>
+  <body>
+    <noscript
+      ><strong
+        >We're sorry but power-mski doesn't work properly without JavaScript
+        enabled. Please enable it to continue.</strong
+      ></noscript
+    >
+    <div id="app"></div>
+  </body>
+</html>
